f99896422b20c3d36b16585987ae3133a9d2d023
Ryan Anderson <ryan0425@msn.com>
2021-04-26 22:54:51 -0700

    quick update

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,43 +12,11 @@ router.get('/', async (req, res) => {
       ],
     });
 
-    const post = postsData.map((post) => post.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', {
-      post,
-    });
+    res.render('homepage', getAllPosts);
+    res.status(200).json(getAllPosts);
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
-router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
-
-  res.render('login');
-});
-
-router.get('/profile', async (req, res) => {
-  try {
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-
 
 module.exports = router;
